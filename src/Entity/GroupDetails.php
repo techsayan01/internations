eada4e9109ef3ff8a667853278c03694bda1f2e2
sayan biswas <techsayan01@gmail.com>
2019-01-30 15:13:51 +0530

    minified database

--- src/Entity/GroupDetails.php
+++ src/Entity/GroupDetails.php
@@ -5,12 +5,12 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * GroupDetails
+ * Groupdetails
  *
- * @ORM\Table(name="group_details", indexes={@ORM\Index(name="group_admin_user_id", columns={"group_admin_user_id"})})
+ * @ORM\Table(name="groupDetails")
  * @ORM\Entity
  */
-class GroupDetails
+class Groupdetails
 {
     /**
      * @var int
@@ -22,13 +22,6 @@ class GroupDetails
     private $groupId;
 
     /**
-     * @var int|null
-     *
-     * @ORM\Column(name="group_pic_id", type="integer", nullable=true)
-     */
-    private $groupPicId;
-
-    /**
      * @var string
      *
      * @ORM\Column(name="group_name", type="string", length=45, nullable=false)
@@ -36,44 +29,15 @@ class GroupDetails
     private $groupName;
 
     /**
-     * @var string|null
+     * @var int
      *
-     * @ORM\Column(name="group_type", type="string", length=45, nullable=true)
-     */
-    private $groupType;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="audit_id", type="string", length=45, nullable=false)
+     * @ORM\Column(name="audit_id", type="integer", nullable=false)
      */
     private $auditId;
-
-    /**
-     * @var \UserDetails
-     *
-     * @ORM\ManyToOne(targetEntity="UserDetails")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="group_admin_user_id", referencedColumnName="user_id")
-     * })
-     */
-    private $groupAdminUser;
 
     public function getGroupId(): ?int
     {
         return $this->groupId;
-    }
-
-    public function getGroupPicId(): ?int
-    {
-        return $this->groupPicId;
-    }
-
-    public function setGroupPicId(?int $groupPicId): self
-    {
-        $this->groupPicId = $groupPicId;
-
-        return $this;
     }
 
     public function getGroupName(): ?string
@@ -88,38 +52,14 @@ class GroupDetails
         return $this;
     }
 
-    public function getGroupType(): ?string
-    {
-        return $this->groupType;
-    }
-
-    public function setGroupType(?string $groupType): self
-    {
-        $this->groupType = $groupType;
-
-        return $this;
-    }
-
-    public function getAuditId(): ?string
+    public function getAuditId(): ?int
     {
         return $this->auditId;
     }
 
-    public function setAuditId(string $auditId): self
+    public function setAuditId(int $auditId): self
     {
         $this->auditId = $auditId;
-
-        return $this;
-    }
-
-    public function getGroupAdminUser(): ?UserDetails
-    {
-        return $this->groupAdminUser;
-    }
-
-    public function setGroupAdminUser(?UserDetails $groupAdminUser): self
-    {
-        $this->groupAdminUser = $groupAdminUser;
 
         return $this;
     }
---
+++ src/Entity/User.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $userId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=45, nullable=false)
+     */
+    private $username;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_admin", type="boolean", nullable=false)
+     */
+    private $isAdmin = '0';
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="audit_id", type="integer", nullable=false)
+     */
+    private $auditId = '0';
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(bool $isAdmin): self
+    {
+        $this->isAdmin = $isAdmin;
+
+        return $this;
+    }
+
+    public function getAuditId(): ?int
+    {
+        return $this->auditId;
+    }
+
+    public function setAuditId(int $auditId): self
+    {
+        $this->auditId = $auditId;
+
+        return $this;
+    }
+
+
+}
