5dd534fd50989f81a5d69d509369fafd82d0c046
sayan biswas <techsayan01@gmail.com>
2019-01-31 14:27:46 +0530

    class declaration

--- src/Controller/AdminGroup/AdminGroupController.php
+++ src/Controller/AdminGroup/AdminGroupController.php
@@ -6,3 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+class AdminGroupController {
+	
+}
